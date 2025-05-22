c1fa50ff3b1a0867ee1137f20842a2860b82f362
lalindu <IT24102173@my.sliit.lk>
2025-05-22 14:37:12 +0530

    created watchedlist class 2

--- src/main/java/com/cinecritix/servlet/DeleteWatchedMovieServlet.java
+++ src/main/java/com/cinecritix/servlet/DeleteWatchedMovieServlet.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 
 @WebServlet("/delete-watched")
-
+//hjhjuhjjhjhj
 public class DeleteWatchedMovieServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
